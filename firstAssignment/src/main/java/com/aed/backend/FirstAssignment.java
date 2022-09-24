ea06a91b9cfac24ad1b875470205cc3b6b17e705
Tarush Singh <tarush.singh29@gmail.com>
2022-09-23 20:04:21 -0400

    Add files via upload

---
+++ firstAssignment/src/main/java/com/aed/backend/FirstAssignment.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.aed.backend;
+
+/**
+ *
+ * @author tarush
+ */
+public class FirstAssignment {
+    private String Name;
+    private String EmployeeID;
+    private String Age;
+    private String Gender;
+    private String StartDate;
+    private String Level;
+    private String Team;
+    private String Position;
+    private String PhoneNumber;
+    private String Email;
+    private String Path;
+
+    public String getPath() {
+        return Path;
+    }
+
+    public void setPath(String Path) {
+        this.Path = Path;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getEmployeeID() {
+        return EmployeeID;
+    }
+
+    public void setEmployeeID(String EmployeeID) {
+        this.EmployeeID = EmployeeID;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(String StartDate) {
+        this.StartDate = StartDate;
+    }
+
+    public String getLevel() {
+        return Level;
+    }
+
+    public void setLevel(String Level) {
+        this.Level = Level;
+    }
+
+    public String getTeam() {
+        return Team;
+    }
+
+    public void setTeam(String Team) {
+        this.Team = Team;
+    }
+
+    public String getPosition() {
+        return Position;
+    }
+
+    public void setPosition(String Position) {
+        this.Position = Position;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+    }
